88d59195dd5552e0ec17e02fe158d00bcc776714
Albert Blanco-Cuaresma <bertobc@users.noreply.github.com>
2018-01-16 23:21:58 +0100

    = environment variable

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,12 @@
-FROM python:3.6.1
+#FROM python:3.6.1
+FROM python
 
 # set working directory
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
+
+# install netcat
+RUN apt-get update && apt-get install -y netcat
 
 # add requirements (to leverage Docker cache)
 ADD ./requirements.txt /usr/src/app/requirements.txt
--- env.sh
+++ env.sh
@@ -1,5 +1,5 @@
 source env/bin/activate
-export REACT_APP_USERS_SERVICE_URL=http://127.0.0.1:5000
+export REACT_APP_USERS_SERVICE_URL=http://127.0.0.1:5001
 export APP_SETTINGS=project.config.DevelopmentConfig
 export DATABASE_URL=postgres://postgres:postgres@localhost:5432/users_dev
 export DATABASE_TEST_URL=postgres://postgres:postgres@localhost:5432/users_test
